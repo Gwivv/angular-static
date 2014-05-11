93d9db70b1d8ea208c565265d728883b7de3b7a2
gwivv <gwivvo@hotmail.fr>
2014-05-11 15:54:37 +0200

    initial commit

---
+++ scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope,$rootScope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS'
+    ];
+  });
